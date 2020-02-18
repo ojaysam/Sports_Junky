418723bc6c0e2443b79ba4a625b99e500fbad409
Samuel Ojo <P2508452@my365.dmu.ac.uk>
2020-02-18 10:34:13 +0000

    Added testing and class for address management

---
+++ dbo.Table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblStaff]
+(
+[StaffID] INT NOT NULL,
+[StaffFirstName] VARCHAR (50) NULL,
+[StaffSurname] VARCHAR (50) NULL,
+[StaffFullName] VARCHAR (50) NULL,
+[WorkingSince] DATE NULL,
+[WorkingToday] BIT NULL,
+[HoursWorked] DECIMAL NULL,
+[StaffAge] INT NULL,
+[CurrentPayRoll] DECIMAL NULL,
+[HolidayDaysLeft] INT NULL, 
+    CONSTRAINT [PK_tblStaff] PRIMARY KEY ([StaffID]),
+
+
+)
