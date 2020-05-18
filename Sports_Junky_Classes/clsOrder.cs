19bfad7a57e80e7ace0368aed8fcba1bb306def4
rokolie <60133153+rokolie@users.noreply.github.com>
2020-05-18 13:00:29 +0100

    Add files via upload

---
+++ Sports_Junky_Classes/clsOrder.cs
@@ -0,0 +1,128 @@
+﻿using ClassLibrary;
+using System;
+
+public class clsOrder
+{
+    private int mOrderId;
+    private DateTime mOrderDate;
+    private string mOrderName;
+    private int mOrderNumber;
+    private int mOrderQuantity;
+    private int mOrderTotal;
+
+    public int OrderId { get => mOrderId; set => mOrderId = value; }
+    public DateTime OrderDate { get => mOrderDate; set => mOrderDate = value; }
+    public string OrderName { get => mOrderName; set => mOrderName = value; }
+    public int OrderNumber { get => mOrderNumber; set => mOrderNumber = value; }
+    public int OrderQuantity { get => mOrderQuantity; set => mOrderQuantity = value; }
+    public int OrderTotal { get => mOrderTotal; set => mOrderTotal = value; }
+
+    public bool Find(int Id) 
+    {
+        clsDataConnection DB = new clsDataConnection();
+        DB.AddParameter("@OrderId",Id);
+        DB.Execute("sproc_tblOrder_FilterByOrderId");
+        if (DB.Count == 1) 
+        {
+            mOrderId = Convert.ToInt32(DB.DataTable.Rows[0]["OrderID "]);
+            mOrderNumber = Convert.ToInt32(DB.DataTable.Rows[0]["OrderNumber"]);
+            mOrderName = Convert.ToString(DB.DataTable.Rows[0]["OrderName "]);
+            mOrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+            mOrderQuantity = Convert.ToInt32(DB.DataTable.Rows[0]["OrderQuantity"]);
+            mOrderTotal = Convert.ToInt32(DB.DataTable.Rows[0]["OrderTotal"]);
+            return true;
+        }
+        return false;
+    }
+
+    public string Valid(string orderNumber, string orderName,string quantity , string orderDate ,string orderTotal) 
+    {
+        string Error = "";
+        try
+        {
+          
+           
+            Int32 orderTemp;
+            Int32 quantityTemp;
+            Int32 totalTemp;
+
+            orderTemp=0;
+            if (orderNumber.Length == 0)
+            {
+                Error += "Order Number Cannot be Empty";
+            }
+            else
+            {
+                if (int.TryParse(orderNumber, out orderTemp))
+                {
+                    if (orderTemp < 0)
+                    {
+                        Error += "\nOrder Number must be greater than 0 :";
+                    }
+                }
+                else
+                {
+                    Error += "Order Number Must be an Integer:";
+                }
+            }
+
+
+
+            if (quantity.Length == 0)
+            {
+                Error += "Order Quantity Cannot be Empty";
+            }
+            else
+            {
+                if (int.TryParse(quantity, out quantityTemp))
+                {
+                    if (quantityTemp < 0)
+                    {
+                        Error += "\nOrder Quantity must be greater than 0 :";
+                    }
+                }
+                else
+                {
+                    Error += "Order Quantity Must be an Integer:";
+                }
+            }
+
+            if (orderTotal.Length == 0)
+            {
+                Error += "Order Total Cannot be Empty";
+            }
+            else
+            {
+                if (int.TryParse(orderTotal, out totalTemp))
+                {
+                    if (totalTemp < 0)
+                    {
+                        Error += "\nOrder Total must be greater than 0 :";
+                    }
+                }
+                else
+                {
+                    Error += "Order Total Must be an Integer:";
+                }
+            }
+
+
+            DateTime DateTemp;
+            DateTemp = Convert.ToDateTime(orderDate);
+            if (DateTemp > DateTime.Now)
+            {
+                Error += "\nDate Cannot be in the future :";
+            }
+            if (DateTemp < DateTime.Now.AddYears(-100))
+            {
+                Error += "\nDate out of range :";
+            }
+        }
+        catch(Exception ex)
+        {
+            Error += "Invalid Date Provided";
+        }
+       
+        return Error;
+    }
+}
---
+++ Sports_Junky_Classes/clsOrderCollection.cs
@@ -0,0 +1,165 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Junky_Classes
+{
+    public class clsOrderCollection
+    {
+        clsDataConnection dBConnection;
+        //private data member for the current address
+        clsOrder thisOrder = new clsOrder();
+        private List<clsOrder> mOrderList;
+
+        public clsOrderCollection()
+        {
+            dBConnection = new clsDataConnection();
+            dBConnection.Execute("sproc_tblOrder_SelectAll");
+            PopulateArray(dBConnection);
+        }
+        public clsOrder ThisOrder
+        {
+            get
+            {
+                return thisOrder;
+            }
+            set
+            {
+                thisOrder = value;
+            }
+        }
+        public Int32 Add()
+        {
+           
+            Int32 PrimaryKey;
+            //create a connection to the database
+            clsDataConnection NewOrder = new clsDataConnection();
+
+            NewOrder.AddParameter("@Number", ThisOrder.OrderNumber.ToString());
+
+            NewOrder.AddParameter("@Name", ThisOrder.OrderName);
+           
+            NewOrder.AddParameter("@Quantity", ThisOrder.OrderQuantity);
+            NewOrder.AddParameter("@Date", ThisOrder.OrderDate);
+            NewOrder.AddParameter("@Total", ThisOrder.OrderTotal);
+
+            //execute the query to add the record - it will return the primary key value of the new record
+            PrimaryKey = NewOrder.Execute("sproc_tblOrder_Insert");
+            //return the primary key value of the new record
+            return PrimaryKey;
+        }
+
+        //function for the public Update method
+        public void Update()
+        {
+         
+            //create a connection to the database
+            clsDataConnection NewOrder = new clsDataConnection();
+
+            NewOrder.AddParameter("@OrderID", ThisOrder.OrderId);
+            NewOrder.AddParameter("@Number", ThisOrder.OrderNumber.ToString());
+
+            NewOrder.AddParameter("@Name", ThisOrder.OrderName);
+
+            NewOrder.AddParameter("@Quantity", ThisOrder.OrderQuantity);
+            NewOrder.AddParameter("@Date", ThisOrder.OrderDate);
+            NewOrder.AddParameter("@Total", ThisOrder.OrderTotal);
+            //execute the query
+            NewOrder.Execute("sproc_tblOrder_Update");
+
+        }
+        public void Delete()
+        ///it is a void function and returns no value
+        {
+            //initialise the DBConnection
+            dBConnection = new clsDataConnection();
+            //add the parameter data used by the stored procedure
+            dBConnection.AddParameter("@OrderId", ThisOrder.OrderId);
+            //execute the stored procedure to delete the address
+            dBConnection.Execute("sproc_tblOrder_Delete");
+        }
+        public void FilterByName(string Name)
+        ///it accepts a single parameter PostCode and returns no value
+        {
+            //initialise the DBConnection
+            dBConnection = new clsDataConnection();
+            
+            dBConnection.AddParameter("@Name", Name);
+               //execute the stored procedure to delete the address
+            dBConnection.Execute("sproc_tblOrder_FilterByName");
+            PopulateArray(dBConnection);
+        }
+
+        void PopulateArray(clsDataConnection DB) 
+        {
+       
+            mOrderList = new List<clsOrder>();
+            Int32 Index = 0;
+            while (Index < dBConnection.Count)
+            {
+                clsOrder NewOrder = new clsOrder();
+                NewOrder.OrderId = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderID "]);
+
+                NewOrder.OrderName = Convert.ToString(dBConnection.DataTable.Rows[Index]["OrderName "]);
+
+                NewOrder.OrderNumber = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderNumber"]);
+
+                NewOrder.OrderQuantity = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderQuantity"]);
+
+                NewOrder.OrderTotal = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderTotal"]);
+
+                NewOrder.OrderDate = Convert.ToDateTime(dBConnection.DataTable.Rows[Index]["OrderDate"]);
+                mOrderList.Add(NewOrder);
+                Index++;
+
+            }
+        }
+
+        public Int32 Count
+        ///it returns the count of records currently in QueryResults
+        {
+            get
+            {
+                //return the count of records
+                return dBConnection.Count;
+            }
+        }
+
+        public List<clsOrder> OrdersList
+        {
+          
+            get
+            {
+                //dBConnection = new clsDataConnection();
+                //List<clsOrder> orderList = new List<clsOrder>();
+                //Int32 Index = 0;
+                //while (Index < dBConnection.Count)
+                //{
+                //    clsOrder NewOrder = new clsOrder();
+                //    NewOrder.OrderId = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderID"]);
+
+                //    NewOrder.OrderName = Convert.ToString(dBConnection.DataTable.Rows[Index]["OrderName"]);
+
+                //    NewOrder.OrderNumber = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderNumber"]);
+
+                //    NewOrder.OrderQuantity = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderQuantity"]);
+
+                //    NewOrder.OrderTotal = Convert.ToInt32(dBConnection.DataTable.Rows[Index]["OrderTotal"]);
+
+                //    NewOrder.OrderDate = Convert.ToDateTime(dBConnection.DataTable.Rows[Index]["OrderDate"]);
+
+                //    Index++;
+
+                //    orderList.Add(NewOrder);
+                    return mOrderList;
+                }
+            set { mOrderList = value; }
+               
+            }
+        }
+
+    }
+
